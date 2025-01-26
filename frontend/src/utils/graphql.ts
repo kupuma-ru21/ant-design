cb1825c860b7774c682265f6a703de0f4b401b3f
kupuma-ru21 <tech.kupumaru@gmail.com>
2025-01-25 22:04:39 -0800

    feat: docker for frontend

--- frontend/src/utils/graphql.ts
+++ frontend/src/utils/graphql.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client/core";
 import { ENV } from "@/envConfig";
 
 export const apolloClient = new ApolloClient({
-  uri: ENV.GRAPHQL_SCHEMA_URL,
+  uri: process.env.GRAPHQL_SCHEMA_URL || ENV.GRAPHQL_SCHEMA_URL,
   cache: new InMemoryCache(),
   defaultOptions: { watchQuery: { fetchPolicy: "cache-and-network" } },
 });
