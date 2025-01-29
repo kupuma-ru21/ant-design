4e1aef524411c9bff08f19c7b597d37bb2ccf544
kupuma-ru21 <tech.kupumaru@gmail.com>
2025-01-28 23:40:48 -0800

    feat: add setting of cloud build for backend

--- backend/main.go
+++ backend/main.go
@@ -61,30 +61,54 @@ func main() {
 		log.Fatal("postgresDb is not set")
 	}
 
+	var postgresqlUrl string
+
+	isDev := env == "local" || env == "docker"
+	if isDev {
+		postgresqlUrl = fmt.Sprintf(
+			"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+			postgresUser, postgresPassword, postgresHost,
+			postgresPort, postgresDb,
+		)
+	} else {
+		// Production
+		// REF: https://cloud.google.com/sql/docs/postgres/connect-instance-cloud-run
+		// REF: https://pkg.go.dev/cloud.google.com/go/cloudsqlconn#section-readme
+		// REF: https://zenn.dev/google_cloud_jp/articles/cloudrun-cloudsql#cloud-run-%E3%82%B5%E3%83%BC%E3%83%93%E3%82%B9%E3%82%92%E4%BD%9C%E6%88%90%E3%81%99%E3%82%8B-1
+		postgresqlUrl = fmt.Sprintf(
+			// NOTE: host=/cloudsql/project:region:instance
+			"host=%s user=%s password=%s dbname=%s",
+			postgresHost, postgresUser,
+			postgresPassword, postgresDb,
+		)
+	}
+
 	// Create ent.Client and run the schema migration.
-	client, err := ent.Open(dialect.Postgres, fmt.Sprintf(
-		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
-		postgresUser, postgresPassword, postgresHost,
-		postgresPort, postgresDb,
-	))
+	client, err := ent.Open(dialect.Postgres, postgresqlUrl)
 	if err != nil {
-		log.Fatal("opening ent client", err)
+		log.Fatal("opening ent client: ", err)
 	}
 	if err := client.Schema.Create(
 		context.Background(),
 		migrate.WithGlobalUniqueID(true),
 	); err != nil {
-		log.Fatal("opening ent client", err)
+		log.Fatal("opening ent client: ", err)
 	}
 
-	// Configure the server and start listening on :8081.
+	// Configure the server and start listening on :8080.
 	srv := handler.NewDefaultServer(resolvers.NewSchema(client))
 	http.Handle("/",
 		playground.Handler("GraphQL playground", "/query"),
 	)
 	http.Handle("/query", srv)
-	log.Println("listening on http://localhost:8081")
-	if err := http.ListenAndServe(":8081", nil); err != nil {
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("port is not set")
+	}
+
+	log.Printf("listening on http://localhost:%s/", port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil); err != nil {
 		log.Fatal("http server terminated", err)
 	}
 }
